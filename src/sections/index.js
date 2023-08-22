c04bea7b0af370df60258c54ecc582b1959414ea
DANKAMN <danielnicholas1477@gmail.com>
2023-08-22 10:32:14 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero"
+import PopularProducts from './PopularProducts'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Subscribe from './Subscribe'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+
+export { Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality, CustomerReviews, Footer }
